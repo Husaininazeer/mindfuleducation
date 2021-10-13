32201ba3174a357faf3fbe8c1cc97d083bcd4001
husaininazeer <husaini.nazeer@gmail.com>
2021-10-13 13:39:58 +0100

    table renders successfully

--- table/src/components/TableRender.js
+++ table/src/components/TableRender.js
@@ -1,34 +1,53 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+import _ from "lodash";
+import { Table, TableHeader } from "semantic-ui-react";
 
 export class TableRender extends Component {
-  static propTypes = {};
-
   state = {
     loading: true,
     data: [],
   };
+  // Reducer function for sorting table columns
 
+  // Function to load component is mounted...
+  // Fetches the data
   async componentDidMount() {
-    console.log("Mounted");
     const url =
       "https://mindfuleducation-cdn.s3.eu-west-1.amazonaws.com/misc/data.json";
     const response = await fetch(url);
     const JSONData = await response.json();
-    // console.log(JSONData.getColleges[0]);
     this.setState({ data: JSONData.getColleges, loading: false });
   }
-
   render() {
     return (
       <div>
         {this.state.loading ? (
           <div>Loading...</div>
         ) : (
-          <div>
-            <div>{this.state.data[0].name}</div>
-            <img src={this.state.data[0].logo}></img>
-          </div>
+          <Table celled fixed>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Name</Table.HeaderCell>
+                <Table.HeaderCell>Prefix</Table.HeaderCell>
+                <Table.HeaderCell>Logo/Preroll</Table.HeaderCell>
+                <Table.HeaderCell>Ofsted Rating</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {this.state.data.map(
+                ({ name, logo, ofstedRating, groupPrefix }) => (
+                  <Table.Row>
+                    <Table.Cell>{name}</Table.Cell>
+                    <Table.Cell>{groupPrefix}</Table.Cell>
+                    <Table.Cell>
+                      <img src={logo} />
+                    </Table.Cell>
+                    <Table.Cell>{ofstedRating}</Table.Cell>
+                  </Table.Row>
+                )
+              )}
+            </Table.Body>
+          </Table>
         )}
       </div>
     );
